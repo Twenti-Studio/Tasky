acba35cbe89014755a026a2924cc4fe78f95e6d5
Noel Ericson Rapael Sipayung <149954229+Noelsip@users.noreply.github.com>
2026-02-02 04:42:48 +0800

    add popup moneteg

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,9 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import Script from "next/script";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import { AuthProvider } from "./context/AuthContext";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +23,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head />
+      <head>
+        <Script
+          src="https://quge5.com/88/tag.min.js"
+          data-zone="207649"
+          strategy="afterInteractive"
+          data-cfasync="false"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen bg-gray-50`}
       >
